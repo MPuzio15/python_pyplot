8c3ae1a43ca3755f5a157f8958c1c496cdea403d
MPuzio15 <martynapuzio93@gmail.com>
2023-12-06 16:58:06 +0100

    too little too much

--- to_little_to_much.py
+++ to_little_to_much.py
@@ -7,19 +7,23 @@ Wskazówka: zapoznaj się z funkcjami zawartymi w bibliotece random. """
 
 from random import randint
 
-x = randint(1, 100)
-print("x: ", x)
-while True:
-    try:
-        user_number = int(input("Jaka liczbe z zakresu <1, 100> wylosowal komputer?:"))
-        if user_number < 1 or user_number > 100:
-            print("To nie jest liczba z tego zakresu")
-        elif user_number > x:
-            print("Za duzo")
-        elif user_number < x:
-            print("Za malo")
-        elif user_number == x:
-            print("Tak! Komputer wylosowal ", x)
-            break
-    except ValueError:
-        print("To nie jest liczba naturalna")
+
+def lets_play():
+    x = randint(1, 100)
+    while True:
+        try:
+            user_number = int(input("Jaka liczbe z zakresu <1, 100> wylosowal komputer?:"))
+            if user_number < 1 or user_number > 100:
+                print("To nie jest liczba z tego zakresu")
+            elif user_number > x:
+                print("Za duzo")
+            elif user_number < x:
+                print("Za malo")
+            elif user_number == x:
+                print("Tak! Komputer wylosowal ", x)
+                break
+        except ValueError:
+            print("To nie jest liczba naturalna")
+
+
+lets_play()
